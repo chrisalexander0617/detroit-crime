f0e9a620fe3dc163076968d70fb534980a2122da
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-12-08 08:43:43 -0500

    Added functions to automate data layer creation for each crime category

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,11 @@
+//https://www.lostcreekdesigns.co/writing/how-to-create-a-map-popup-component-using-mapbox-and-react/
 
 import React, {useEffect, useState, useRef} from "react";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 import {createDataLayer} from './functions'
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN
 import crimeData from '../../features/api/crimeData.json'
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN
 
 const crimeDataLayer = crimeData.features.map((crime, i) => (
   {
@@ -26,34 +27,37 @@ const crimeDataLayer = crimeData.features.map((crime, i) => (
       "neighborhood":crime.attributes.neighborhood,
       "precinct":crime.attributes.precinct,
       "oid":crime.attributes.oid
-
     }
   }
 ))
 
-const robberyDataLayer = {
-  'type': 'FeatureCollection',
-  'features':crimeDataLayer.filter(crime => crime.properties.offense_category === "ROBBERY")
+const getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
+/* funciton that takes all crime
+ and generates a data layer for each of them */
+const createDataLayerFromJSON = (mapInstance, data) => {
+  const crimeCategoriesRaw = []
 
-const fraudDataLayer = {
-  'type': 'FeatureCollection',
-  'features':crimeDataLayer.filter(crime => crime.properties.offense_category === "FRAUD")
-}
+  data.forEach((crime, i) => {
+    crimeCategoriesRaw.push(crime.properties.offense_category)
+  })
 
-const arsonDataLayer = {
-  'type': 'FeatureCollection',
-  'features':crimeDataLayer.filter(crime => crime.properties.offense_category === "ARSON")
-}
+  const crimeCategories = [...new Set(crimeCategoriesRaw)]
 
-const burglaryDataLayer = {
-  'type': 'FeatureCollection',
-  'features':crimeDataLayer.filter(crime => crime.properties.offense_category === "BURGLARY")
-}
+  crimeCategories.forEach((category) => {
+    const dataLayer = {
+      'type': 'FeatureCollection',
+      'features': data.filter(crime => crime.properties.offense_category === category)
+    }
 
-const autoTheftDataLayer = {
-  'type': 'FeatureCollection',
-  'features':crimeDataLayer.filter(crime => crime.properties.offense_category === "STOLEN VEHICLE")
+    mapInstance.addLayer(createDataLayer(category, dataLayer, getRandomColor()));
+  })
 }
 
 export const Map = () => {
@@ -63,8 +67,6 @@ export const Map = () => {
     const [lat, setLat] = useState(42.33);
     const [zoom, setZoom] = useState(13);
 
-    
-    //https://www.lostcreekdesigns.co/writing/how-to-create-a-map-popup-component-using-mapbox-and-react/
     const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }))
     const mapImageOne = 'icons/fraud.png'
 
@@ -73,7 +75,6 @@ export const Map = () => {
             container: mapContainer.current,
             style: 'mapbox://styles/christopherclemmons2020/cl37v6xlr000u14mlzlfbuc80',
             zoom: zoom,
-            // lng lat
             center:[lng, lat]
         });
 
@@ -87,21 +88,22 @@ export const Map = () => {
                     map.current.addImage('map-image', image);
                 }
             );
-       
-            map.current.addLayer(createDataLayer('robbery', robberyDataLayer, '#3dd6f5'));
-            map.current.addLayer(createDataLayer('fraud', fraudDataLayer, '#cfeb34'));
-            map.current.addLayer(createDataLayer('arson', arsonDataLayer, '#e55e5e'));
-            map.current.addLayer(createDataLayer('burglary', burglaryDataLayer, '#ffffff'));
-            map.current.addLayer(createDataLayer('auto theft', autoTheftDataLayer, '#f5993d'));
+              
+            createDataLayerFromJSON(map.current, crimeDataLayer)
 
             map.current.addControl(
                 new mapboxgl.GeolocateControl({
                     positionOptions: {
                         enableHighAccuracy: true
                     },
-                    // When active the map will receive updates to the device's location as it changes.
+                    /* When active the map will receive updates 
+                    to the device's location as it changes.
+                    */
                     trackUserLocation: true,
-                    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+
+                    /* Draw an arrow next to the location 
+                    dot to indicate which direction the device is heading.
+                    */
                     showUserHeading: true
                 })
             );
